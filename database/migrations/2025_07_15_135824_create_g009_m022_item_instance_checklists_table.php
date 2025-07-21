27bcca48e280c34ba9c7b3fc509f25210290a17b
Yusuf Bayu Wicaksono <yusuf.wicaksono@tarunabakti.or.id>
2025-07-21 12:35:31 +0700

    short fk 2

--- database/migrations/2025_07_15_135824_create_g009_m022_item_instance_checklists_table.php
+++ database/migrations/2025_07_15_135824_create_g009_m022_item_instance_checklists_table.php
@@ -13,13 +13,23 @@ return new class extends Migration
     {
         Schema::create('g009_m022_item_instance_checklists', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('g002_m015_item_instance_id')->nullable()->constrained('g002_m015_item_instances')->cascadeOnDelete();
+            $table->foreignId('g002_m015_item_instance_id')->nullable();
             $table->foreignId('user_id')->nullable()->constrained('users')->cascadeOnDelete();
             $table->date('date')->nullable();
             $table->text('notes')->nullable();
             $table->string('photo')->nullable();
             $table->dateTime('checklist_date')->nullable();
             $table->timestamps();
+
+            $table->foreignUuid('g005_m009_item_reservation_id')->nullable();
+            $table->foreignId('g002_m015_item_instance_id')->nullable();
+            $table->timestamps();
+
+            // Define foreign key with a shorter custom name
+            $table->foreign('g002_m015_item_instance_id', 'fk_item_checklist_to_instances')
+                ->references('id')
+                ->on('g002_m015_item_instances')
+                ->cascadeOnDelete();
         });
     }
 
